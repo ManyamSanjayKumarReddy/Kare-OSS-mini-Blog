18c1a79887915ca54a28827b5018177ec334ae2c
Manyam Sanjay Kumar Reddy <94047241+ManyamSanjayKumarReddy@users.noreply.github.com>
2023-02-19 17:55:42 +0530

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -10,5 +10,3 @@ urlpatterns = [
     path('delete-blog/<str:pk>/', views.delete, name='delete-blog')
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
